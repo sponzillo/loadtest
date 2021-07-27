213065d39f71292eb66ec4a9b97f23e425c249fa
Andrea Sponziello <32564846+sponzillo@users.noreply.github.com>
2021-07-27 17:27:56 +0000

    min

--- app.js
+++ app.js
@@ -5,15 +5,20 @@ app.get('/', (req, res) => {
     res.send('Hello');
 });
 
+var count = 0;
 // first api which just responds to the request without any processing
 app.get('/ping', (req, res) => {
-  console.log("/ping");
+  console.log("/ping: " + count);
+  count++;
   res.send('pong');
 });
 
+
+var hcount = 0;
 // second api which waits for 50 millis before responding 
 app.get('/heavy-ping', (req, res) => {
-  console.log("/ping");
+  console.log("/heavy-ping: " + hcount);
+  hcount++;
   setTimeout(() => {
     console.info("eating time for demo");
     res.send('heavy pong');
